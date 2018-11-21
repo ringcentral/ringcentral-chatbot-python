4c4b2eacc22e73e3ec89477be4b3554a0c954878
ZHAO Xudong <zxdong@gmail.com>
2018-11-21 14:20:23 +0800

    fix #27 Add Use CLI tool to create an bot app section

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ringcentral_bot_framework",
-    version="0.0.3",
-    author="Drake Zhao",
+    version="0.0.4",
+    author="Drake Zhao @ RingCentral",
     author_email="drake.zhao@ringcentral.com",
     description="RingCentral Chatbot Framework for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zxdong262/ringcentral-chatbot-python",
     packages=setuptools.find_packages(),
+    keywords=['ringcentral', 'bot', 'framework'],
+    install_requires=[i.strip() for i in open('requirements.txt').readlines()],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
