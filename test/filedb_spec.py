ff4147d9b3437284902270df2cf8c291f5a71ce4
ZHAO Xudong <zxdong@gmail.com>
2018-11-22 13:59:25 +0800

    fix

--- test/filedb_spec.py
+++ test/filedb_spec.py
@@ -7,25 +7,26 @@ class TestFiledbMethods(unittest.TestCase):
 
   def test_basic_filedb(self):
     self.assertEqual(DBNAME, 'filedb')
-    x = action('bot', 'add', {'s': 's', 'id': 'xss', 'dgfdf': [
+    x = action('bot', 'add', {'s': 's', 'id': 'axx', 'dgfdf': [
       'sdfs', 'sdfsd'
     ]})
     self.assertEqual(x, 'add')
-    x1 = action('bot', 'add', {'s': 's', 'id': 'xss1', 'dgfdf': [
+    x1 = action('bot', 'add', {'s': 's', 'id': 'axx1', 'dgfdf': [
       'sdfs', 'sdfsd'
     ]})
     self.assertEqual(x1, 'add')
-    x2 = action('bot', 'get', { 'id': 'xss1'})
-    self.assertEqual(x2['id'], 'xss1')
-    x2 = action('bot', 'update', { 'id': 'xss1', 'update': {
+    x2 = action('bot', 'get', { 'id': 'axx1'})
+    self.assertEqual(x2['id'], 'axx1')
+    x2 = action('bot', 'update', { 'id': 'axx1', 'update': {
       's': 45,
       'fg': 'sdf'
     }})
-    x2 = action('bot', 'get', { 'id': 'xss1'})
-    self.assertEqual(x2['id'], 'xss1')
+    x2 = action('bot', 'get', { 'id': 'axx1'})
+    self.assertEqual(x2['id'], 'axx1')
     self.assertEqual(x2['s'], 45)
     self.assertEqual(x2['fg'], 'sdf')
     x2 = action('bot', 'get')
     self.assertEqual(x2[1]['fg'], 'sdf')
+
 if __name__ == '__main__':
     unittest.main()
