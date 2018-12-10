769ab123e7574daa4578f9b5055e11942136aa07
ZHAO Xudong <zxdong@gmail.com>
2018-12-10 09:35:02 +0800

    fix #47 Disable dataviewer by default

--- ringcentral_bot_framework/core/data.py
+++ ringcentral_bot_framework/core/data.py
@@ -6,10 +6,19 @@ from .db import dbAction
 from .config import configAll
 from .common import result
 import json
+import os
 
 tables = list(map(lambda x: x['name'], configAll.dbTables()))
 
+disabled = True
+try:
+  disabled = os.environ['DATA_VIEWER_ENABLED'] != 'yes'
+except:
+  pass
+
 def dataView(event):
+  if disabled:
+    return result('Data viewer diabled, set DATA_VIEWER_ENABLED=yes to enable it')
   tableName = _.get(event, 'queryStringParameters.tableName') or 'bot'
   if not tableName in tables:
     return result('tableName not right', 400)
