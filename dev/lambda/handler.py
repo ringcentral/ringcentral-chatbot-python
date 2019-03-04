324830004cf543dccab6fb285c9a761593d78f85
ZHAO Xudong <zxdong@gmail.com>
2019-03-04 08:16:01 +0800

    ver 2.0b1

--- dev/lambda/handler.py
+++ dev/lambda/handler.py
@@ -1,7 +1,13 @@
 
-from ringcentral_bot_framework import router
+from ringcentral_bot_framework import frameworkInit
+import config as conf
+
+# import ringcentral_bot_framework_extension_botinfo as botinfo
+# import ringcentral_bot_framework_extension_world_time as wt
+# framework = frameworkInit(conf, [botinfo, wt])
+
+framework = frameworkInit(conf)
 
 def bot(event, context):
 
-    return router(event)
-
+  return framework.router(event)
